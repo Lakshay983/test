2f6a08e4911cadbf3d2baee670adadcc9b754a5b
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2020-10-09 12:41:43 -0500

    Update Matthew.py

--- Matthew.py
+++ Matthew.py
@@ -1,10 +1,27 @@
+def match(first, second): 
+    if len(first) == 0 and len(second) == 0: 
+        return True
+    if len(first) > 1 and first[0] == '*' and  len(second) == 0: 
+        return False
+    if (len(first) > 1 and first[0] == '?') or (len(first) != 0
+        and len(second) !=0 and first[0] == second[0]): 
+        return match(first[1:],second[1:]); 
+    if len(first) !=0 and first[0] == '*': 
+        return match(first[1:],second) or match(first,second[1:]) 
+    return False
 with open('Matthew.dat', 'r', encoding="utf-8") as file:
     while True: 
-        line = file.readline() 
+        line = file.readline().split()
         if not line: 
             break
         num=int(file.readline())
-        list=[]
-        for x in range(num)
-            reg=file.readline()
-            #do stuff
+        matches=""
+        for x in range(num):
+            reg=file.readline().partition('\n')[0]
+            for i in line:
+                if(match(reg,i)):
+                    matches+=(i+" ")
+            if matches=="":
+                print("None")
+            else:
+                print(matches)
