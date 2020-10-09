c9a42a95ffbe6b3eb8b06afa85ed2a12a78a4633
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2020-10-09 12:19:28 -0500

    Update Matthew.py

--- Matthew.py
+++ Matthew.py
@@ -1,1 +1,10 @@
-
+with open('Matthew.dat', 'r', encoding="utf-8") as file:
+    while True: 
+        line = file.readline() 
+        if not line: 
+            break
+        num=int(file.readline())
+        list=[]
+        for x in range(num)
+            reg=file.readline()
+            #do stuff
